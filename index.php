4fea3a0c9a1ba0d78d75bcaa3e8ccbd340c30791
Tim George <tim@timrgeorge.com>
2022-09-14 15:48:21 -0400

    Reinitialization commit

---
+++ index.php
@@ -0,0 +1,265 @@
+<!doctype html>
+
+<!--[if lt IE 7 ]> <html class="ie ie6 ie-lt10 ie-lt9 ie-lt8 ie-lt7 no-js" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 ie-lt10 ie-lt9 ie-lt8 no-js" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 ie-lt10 ie-lt9 no-js" lang="en"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 ie-lt10 no-js" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+
+<!-- ************************************************************************ -->
+<!--                                                                          -->
+<!-- Hey, thanks for checking out my source code and seeing what              -->
+<!-- I'm all about. If you like what you see, please feel free to             -->
+<!-- get in touch with me at: tim@timrgeorge.com                              -->
+<!--                                                                          -->
+<!-- Thanks again for coming by!                                              -->
+<!-- For your efforts, please enjoy the following:                            -->
+<!-- https://www.youtube.com/watch?v=AZKpByV5764                              -->
+<!-- or                                                                       -->
+<!-- https://www.youtube.com/watch?v=3Sso3h0xMJA                              -->
+<!--                                                                          -->
+<!-- Orbitron Font:       https://www.theleagueofmoveabletype.com/orbitron    -->
+<!-- Initial Templates:   https://github.com/murtaugh/HTML5-Reset             -->
+<!--                                                                          -->
+<!-- ************************************************************************ -->
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+<title>Resume | Tim George</title>
+<meta name="author" content="Tim George" />
+<meta name="description" content="Resume of Tim George - UI/UX &amp; Web Developer." />
+<meta name="Copyright" content="<?php echo date('Y')?> - Tim George" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="jjAew9bmyHsT_U5BIFoSsjSd01R96zaSarkJjZJB894" />
+
+<link rel="shortcut icon" href="assets/img/favicon.ico" />
+<link rel="stylesheet" href="assets/css/style.css" />
+
+<script src="assets/js/libs/modernizr-2.8.3.js"></script>
+</head>
+
+<body>
+
+<div class="wrapper">
+
+<header>
+<a class="logoLink" href="http://timrgeorge.com/"><h1 class="king">TIM<span class="colorText">GEORGE</span></h1><h2>Resume</h2></a> 
+</header>
+
+<section class="content skills">
+<h2>Skills</h2>
+<p><strong>Markup / Programming Languages &amp; Libraries</strong><br />HTML, XML, CSS, JavaScript, jQuery, Backbone.js, RequireJS, Spring MVC, Apache Velocity, Apache Tiles, PHP, ASP, ASP.NET, VB, MySQL, MS SQL, Oracle DBMS, Java, C++, C#</p>
+<p><strong>Blog / CMS / Platforms:</strong><br />Drupal, Perch, WordPress, Ektron CMS 400, SharePoint</p>
+<p><strong>Version Control:</strong><br />GIT, SVN, TFS, VSS</p>
+<p><strong>Operating Systems:</strong><br />Windows (8/7/Vista/XP/9X), Mac OSX</p>
+<p><strong>Software Packages:</strong><br />Visual Studio / VS Code, Eclipse, PHPStorm, Spring Tool Suite, Microsoft Office (Word, Excel, Access, PowerPoint, FrontPage, Publisher, Visio, Visual Studio), Adobe Creative Suite (Fireworks, Photoshop, Illustrator), Adobe Director, Pro Tools 7LE</p>
+<p class="no-padding"><strong>Other:</strong></p>
+<ul>
+<li>Enjoys using techniques such as Progressive Enhancement and Responsive Web Design</li>
+<li>Work well on projects in a team setting, both in leadership or support role</li>
+<li>Excellent oral and written communication skills</li>
+<li>Effective, organized and able to multi-task</li>
+<li>Work well with minimum supervision</li>
+</ul>
+</section>
+
+<section class="content experience">
+<h2>Experience</h2>
+
+<p>
+<strong>SimpliSafe</strong>
+<small>Boston, MA  -  September 2019 to Present</small>
+</p>
+<p class="job-title"><em>Software Engineer</em></p>
+<ul>
+<li>Streamlined and revamped the process for enabling &amp; configuring promotions on the E-Commerce website</li>
+<li>Implemented a front-end system for building &amp; customizing a security system to purchase.</li>
+<li>Support and maintainenance of the checkout workflow, worked with payment providers and financing systems for end-users to utilize.</li>
+<li>Worked with and implemented a wide range of microservices to interact with our main CRM system.</li>
+</ul>
+
+<p>
+<strong>Eaton Vance</strong>
+<small>Boston, MA  -  May 2015 to September 2019</small>
+</p>
+<p class="job-title"><em>Senior Web Developer</em></p>
+<ul>
+<li>Responsible for support and maintenance for intenal marketing configuration applications</li>
+<li>Wrote new common framework system to unite all internal applications with consistent UI patterns</li>
+<li>Swapped configuration applications from an Oracle DMBS to Microsoft SQL systems</li>
+<li>Converted exisitng database calls into RESTful web services utilizing JSON data format</li>
+<li>Wrote new libraries to tie into lead-generation API services like Email Hunter and helped to automate the updating of our internal records and CRM systems.</li>
+</ul>
+
+<p>
+<strong>Boston Financial Data Services</strong>
+<small>Quincy, MA  -  October 2009 to May 2015</small>
+</p>
+
+<p class="no-padding">
+<em>Managing UI/UEX Architect; E-Business Consulting</em>
+<small>December 2013 to May 2015</small>
+</p>
+<ul>
+<li>Responsible for consulting with internal/external clients and proposing user experience solutions</li>
+<li>Lead a team of UI/UEX consultants that played a critical role in the planning of site content and functionality</li>
+<li>Collaborated with the development &amp; support teams to ensure proper translation of the overall strategy/vision</li>
+<li>Continued role of Lead Technical Consultant, providing guidance/mentoring to development team</li>
+<li>Researched and implemented front-end frameworks and technologies for new Secure Account Access website, including Backbone.js, Modernizr, jQuery UI, and RequireJS</li>
+<li>Coded transactional workflows for new application utilizing RESTful services created by middleware development team</li>
+<li>Used VTL (Velocity Templating) engine to custom tailor JSON response objects on a per client basis</li>
+</ul>
+
+<p class="job-title">
+<em>Lead Technical Consultant / Web &amp; UI Developer; E-Business Consulting</em>
+<small>March 2012 to December 2013</small>
+</p>
+<ul>
+<li>Worked within new E-Business initiative group to provide clients with new services and opportunities</li>
+<li>Created new Secure Account Access site while working with a middleware development team. Technologies used to create this included: Spring MVC, Apache Velocity, Apache Tiles, HTML5, CSS, JavaScript</li>
+<li>Researched JavaScript frameworks, including Angular.js, Backbone.js, and Knockout.js</li>
+<li>Developed mobile sites, using responsive web design practices, while focusing on content first</li>
+<li>Used progressive enhancement to serve HTML5 and CSS3 elements, and still be compatible in older browsers</li>
+</ul>
+
+<p class="job-title">
+<em>Web Developer - Contract Employment through Kelly Services</em>
+<small>September 2011 to March 2012</small>
+</p>
+<ul>
+<li>Worked remotely from Pennsylvania as a Web Developer Contractor</li>
+<li>Developed a public mobile site for client utilizing HTML5 / CSS Media Queries</li>
+<li>Administered updates to Intranet, Extranet, and Internet sites</li>
+</ul>
+
+
+<p class="job-title">
+<em>Web Developer</em>
+<small>October 2009 to September 2011</small>
+</p>
+<ul>
+<li>Maintained and updated templates for ten different (but similar) sites of one of our clients</li>
+<li>Modified templates existing code to meet current web standards</li>
+<li>Learned the importance of managing time across multiple facets of the same project</li>
+</ul>
+
+<p>
+<strong>True Action, an eBay Enterprise Company</strong>
+<small>King of Prussia, PA  -  September 2011 to February 2012</small>
+</p>
+<p class="job-title"><em>Web Developer</em></p>
+<ul>
+<li>Developed cross-compatible HTML email templates for multiple clients</li>
+<li>Developed interactive solutions that were HTML5 compatible to work on iPads</li>
+<li>Gained knowledge of working with and updating code across multiple repositories</li>
+<li>Learned the importance of a QA and Release Management system to identify/prevent issues</li>
+<li>Cultivated important relationships within all development teams to ensure a common goal</li>
+</ul>
+
+<p>
+<strong>Aramark Uniform Services (WearGuard)</strong>
+<small>Norwell, MA  -  October 2008 to October 2009</small>
+</p>
+<p class="job-title"><em>Web Developer</em></p>
+<ul>
+<li>Revamped existing template files in an effort to make them cross-browser compatible</li>
+<li>Designed and developed a blogging platform for corporate use</li>
+<li>Modified and implemented existing code to meet current web standards</li>
+<li>Gained additional knowledge of administering an ecommerce website</li>
+</ul>
+
+
+<p id="work-toggle"><a href="" title="View Additional Work Experience">View Additional Work Experience &raquo;</a></p>
+
+<div id="additional-work">
+<p>
+<strong>Kelly Services - Boston Financial Data Services</strong>
+<small>North Quincy, MA  -  June 2008 to September 2008</small>
+</p>
+<p class="job-title"><em>Web Technologies Contractor</em></p>
+<ul>
+<li>Administered updates to Intranet, Extranet, and Internet sites</li>
+<li>Created inventory databases as well as client web applications</li>
+<li>Participated in many teamwork strategies and gained significant experience in a corporate setting</li>
+</ul>
+
+<p>
+<strong>Kelly Services - Boston Financial Data Services</strong>
+<small>North Quincy, MA  -  June to August 2007</small>
+</p>
+<p class="job-title"><em>Web Technologies Internship</em></p>
+<ul>
+<li>Administered updates to Intranet, Extranet, and Internet sites</li>
+<li>Created inventory databases as well as client web applications</li>
+<li>Participated in many teamwork strategies and gained significant experience in a corporate setting</li>
+</ul>
+
+<p>
+<strong>The Gillette Company</strong>
+<small>Boston, MA  -  June to August 2006</small>
+</p>
+<p class="job-title"><em>Summer Intern for Business Management Unit</em></p>
+<ul>
+<li>Assisted with the integration of Gillette into the Procter &amp; Gamble company</li>
+<li>Utilized existing software to develop an easy-to-use application for database comparisons</li>
+<li>Learned the importance of clear, accurate and timely documentation</li>
+</ul>
+
+<p>
+<strong>Adecco - Boston Financial Data Services</strong>
+<small>Braintree, MA  -  June to August 2005</small>
+</p>
+<p class="job-title"><em>Web Technologies Internship</em></p>
+<ul>
+<li>Administered updates to Intranet, Extranet, and Internet sites</li>
+<li>Created inventory databases as well as client web applications</li>
+<li>Participated in many teamwork strategies and gained significant experience in a corporate setting</li>
+</ul>
+</div>
+</section>
+
+<section class="content education">
+<h2>Education</h2>
+<p><strong>Bachelor of Science</strong> in Information Technology from the <strong><a href="http://www.rit.edu/">Rochester Institute of Technology</a></strong><br />Concentrations in Web Development and Online Digital Media - <em>Minored in Music</em><br />Rochester, NY</p>
+<p><strong>Relevant Courses:</strong><br /> Programming in Java I, II, &amp; III, Computer Concepts and Software Systems, Human Computer Interactions I &amp; II, Needs Assessment, Technology Transfer, Data Communications &amp; Networking, Networking Fundamentals, Intro to Database and Data Modeling, Web Site Design &amp; Implementation, Web Client Side Programming, Programming for World Wide Web, Interactive Multimedia Development, Programming for Digital Media, Digital Audio &amp; Computer Music, Foundations of 2D Graphics Programming</p>
+</section>
+
+<section class="content">
+<h2>Hobbies &amp;<br />Activities</h2>
+<ul>
+<li>Twitch Affiliate / Variety Streamer - enjoy creating content as a creative outlet</li>
+<li>Saxophonist &amp; Bass Clarinetist, enjoys playing with a variety of different bands</li>
+<li>Building &amp; repairing computers - avid video-gamer</li>
+<li>Building Legos</li>
+</ul>
+</section>
+
+<!-- <section class="content">
+<h2>Download</h2>
+<p><a href="resume.pdf">PDF Version</a> available for download.</p>
+</section> -->
+
+<footer>
+<p>Copyright &copy; 2009 - <?php echo date('Y'); ?> <a href="http://timrgeorge.com">timrgeorge.com</a> - All Rights Reserved.</p>
+</footer>
+
+</div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script>window.jQuery || document.write("<script src='assets/js/libs/jquery-1.11.0.min.js'>\x3C/script>")</script>
+<script src="assets/js/functions.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-709645-11', 'auto');
+  ga('send', 'pageview');
+</script>
+
+</body>
+</html>
